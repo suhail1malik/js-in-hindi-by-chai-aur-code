48c1dce0505e07e5b26459568f7db112f4871d62
suhail1malik <73252701+suhail1malik@users.noreply.github.com>
2024-07-24 07:04:30 +0000

    javascript

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let employName = "Suhail Mlaik" ;
+var city = "shamli" ;
+state = "uttar Pradesh" ;
+let country ;
+
+// accountId = 4353 ;  This is not allowed in js we can not modify const value 
+//  dont use "var" now its not prefer because it has block scope issue and functional scope issue
+// if we did not assingn any value in variable then we get undifiened value by default 
+
+console.table([ accountId, employName, city, state, country ]) ;
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all code as newer version code 
+
+// Primitive data type
+
+let name = "Suhail "; // string data type 
+let num = 1234 ;   // number data type 
+// boolean  -> use for true/ false value only 
+// bigint for big numbers
+// null -> standalone value 
+// undefiened -> value not assigned till now
+// symbol for uniqueness 
+
+// typeof null is equal to  object
+// type of undefined is undefined
+
+// object data type
